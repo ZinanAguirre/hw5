1fdd59c0b843c238194d8be463ec991ee368ccd0
Zinan Aguirre <zaaguirr@usc.edu>
2025-04-17 07:46:27 +0000

    Submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool recursive(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<size_t>& workedShifts);
 
+bool isValid (const size_t maxShifts, size_t worker, vector<size_t>& workedShifts,DailySchedule& sched,size_t rows);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +40,50 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t days = avail.size();
+    size_t workers = avail[0].size();
+    vector<size_t> workedShifts(workers,0);
+    sched = DailySchedule(days, vector<Worker_T>(dailyNeed, INVALID_ID));
+
+    return recursive(0,0,dailyNeed,avail, maxShifts, sched, workedShifts);
 
 
+}
 
+bool recursive(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<size_t>& workedShifts)
+{
+  if (rows == sched.size()){
+    return true;
+  }
+
+  if(cols == dailyNeed){
+    return recursive(rows+1, 0, dailyNeed, avail, maxShifts, sched, workedShifts);
+  }
+
+  for(size_t i = 0; i < workedShifts.size(); ++i){
+      if(avail[rows][i] == 1){
+          sched[rows][cols] = i;
+          workedShifts[i] += 1;
+          if(isValid(maxShifts, i, workedShifts, sched, rows)){
+              if(recursive(rows, cols+1,dailyNeed,avail,maxShifts,sched,workedShifts)){
+                return true;
+              }
+          }
+          workedShifts[i] -= 1;
+          sched[rows][cols] = INVALID_ID;
+      }
+  }
+
+  return false;
+}
+
+
+bool isValid (const size_t maxShifts, size_t worker,vector<size_t>& workedShifts,DailySchedule& sched,size_t rows)
+{
+  if(workedShifts[worker] > maxShifts){
+    return false;
+  }
+
+  return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+
+
 #endif
 
 #include "wordle.h"
@@ -13,7 +15,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void potentialWordsHelper (size_t size, std::string& current, const std::set<std::string>& dict, const std::string& floating,std::string& now, std::set<std::string>& options,const std::string& in);
+bool isValid(std::string potentialWord, const std::string& floating,const std::string& in);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +25,92 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> possibleWords;
+    std::string current = in;
+    std::string now = floating;
+    potentialWordsHelper(0, current, dict, floating, now, possibleWords, in);
+    return possibleWords;
+    
 }
 
 // Define any helper functions here
+
+void potentialWordsHelper(size_t position, std::string& current, const std::set<std::string>& dict,const std::string& floating,  std::string& now, std::set<std::string>& options, const std::string& in) 
+{
+    if(current.size() == 0){
+      return;
+    }
+    
+    if((current.size()) == position)
+    {
+      if(isValid(current, now, in)){
+        if(dict.find(current) != dict.end()){
+          options.insert(current);
+        } 
+      }
+      return;
+    }
+
+    if(current[position] == '-'){
+       /* for(size_t i = 0; i < floating.size(); ++i){
+          current[position] = floating[i];
+          std::string now = floating;
+          now.erase(i,1);
+          potentialWordsHelper(position + 1, current, dict, now, options);
+        }
+        if(floating.size() < (current.size() - position)){
+          for(char place = 'a'; place <= 'z'; ++place){
+            if(floating.find(place) == std::string::npos) {
+              current[position] = place;
+              potentialWordsHelper(position + 1, current, dict, floating, options);
+            } 
+          }
+        }
+        current[position] = '-';*/
+
+        for(size_t i = 0; i < floating.size(); ++i){
+          current[position] = floating[i];
+          std::string on = floating;
+          on.erase(i,1);
+          potentialWordsHelper(position + 1, current, dict, on, now, options, in);
+        }
+        if(floating.size() < (current.size() - position)){
+          for(char place = 'a'; place <= 'z'; ++place){
+            if(floating.find(place) == std::string::npos) {
+              current[position] = place;
+              potentialWordsHelper(position + 1, current, dict, floating,now, options, in); 
+            }
+          }
+        }
+        current[position] = '-';
+
+        /*for(char place = 'a'; place <= 'z'; place++){
+           size_t location = now.find(place);
+           if(location != std::string::npos){
+              current[position] = now[location];
+               now.erase(location,1);
+               potentialWordsHelper(position + 1, current, dict,floating, now, options);
+           }
+           else{
+            current[position] = place;
+            potentialWordsHelper(position + 1, current, dict, floating,now, options);
+           }
+        }
+        current[position] = '-';*/
+    }
+    else{
+      potentialWordsHelper(position + 1, current, dict, floating,now, options, in);
+    }
+
+}
+bool isValid(std::string potentialWord, const std::string& floating, const std::string& in) 
+{
+  for(size_t i = 0; i < floating.size(); ++i){
+    size_t location = potentialWord.find(floating[i]);
+    if(location == std::string::npos){
+      return false;
+    }
+    potentialWord[location] = '-';
+  }
+  return true;
+}
