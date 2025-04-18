9cd9d2c499d34421605c9ebe1d51867ddd32e8df
Zinan Aguirre <zaaguirr@usc.edu>
2025-04-18 04:58:37 +0000

    Final Submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-bool recursive(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<size_t>& workedShifts);
 
-bool isValid (const size_t maxShifts, size_t worker, vector<size_t>& workedShifts,DailySchedule& sched,size_t rows);
+/* finalSchedule recursion function that will backtrack and try all possible combinations for workers to work in a maxShifts.
+This will have a similar style to a soduko game backtracking using a 2d array and checking if certain users are valid.
+*/
+bool finalSchedule(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<size_t>& workedShifts);
+
+// An insertion will be valid as long as the worker when they are inserted to the shift does not over pass the maxshifts 
+bool isValid (const size_t maxShifts, size_t worker, vector<size_t>& workedShifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -41,34 +46,46 @@ bool schedule(
     // Add your code below
 
     size_t days = avail.size();
+    // number of workers
     size_t workers = avail[0].size();
+    //A vector to keep track how many shifts each worker has worked
     vector<size_t> workedShifts(workers,0);
+    //Assign each day and workers working that day to -1 so that we could fix and insert their worker Id properly.
     sched = DailySchedule(days, vector<Worker_T>(dailyNeed, INVALID_ID));
 
-    return recursive(0,0,dailyNeed,avail, maxShifts, sched, workedShifts);
+    return finalSchedule(0,0,dailyNeed,avail, maxShifts, sched, workedShifts);
 
 
 }
 
-bool recursive(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<size_t>& workedShifts)
+bool finalSchedule(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<size_t>& workedShifts)
 {
+  //Once all rows are completed then we are done since all days of the schedule have been divided.
   if (rows == sched.size()){
     return true;
   }
 
+// Once we have determine the daily need of workers needed for the day we go on to the next day and start that row by filling in the workers again.
   if(cols == dailyNeed){
-    return recursive(rows+1, 0, dailyNeed, avail, maxShifts, sched, workedShifts);
+    return finalSchedule(rows+1, 0, dailyNeed, avail, maxShifts, sched, workedShifts);
   }
 
+// Use the vectorsize of the worked shifts to iteratate through each worker in the company.
   for(size_t i = 0; i < workedShifts.size(); ++i){
+      //First we will check if the worker is available that day, if so then they are a potential candidiate to work and save time if they cant work.
       if(avail[rows][i] == 1){
+          // We will palce their id in the schedule.
           sched[rows][cols] = i;
+          // Then add one to show the shift they are about to work in.
           workedShifts[i] += 1;
-          if(isValid(maxShifts, i, workedShifts, sched, rows)){
-              if(recursive(rows, cols+1,dailyNeed,avail,maxShifts,sched,workedShifts)){
+          // Now we have to check if this insertion of worker to work on that day is valid and capable.
+          if(isValid(maxShifts, i, workedShifts)){
+            // If they are able to work and it is valid then we go onto the next slot for worker we could have to work on that day.
+              if(finalSchedule(rows, cols+1,dailyNeed,avail,maxShifts,sched,workedShifts)){
                 return true;
               }
           }
+          // if it was not valid, then we have to redo the opertions such as remove their shift value and put back id to -1, and check the next worker.
           workedShifts[i] -= 1;
           sched[rows][cols] = INVALID_ID;
       }
@@ -78,7 +95,8 @@ bool recursive(size_t rows, size_t cols, size_t dailyNeed, const AvailabilityMat
 }
 
 
-bool isValid (const size_t maxShifts, size_t worker,vector<size_t>& workedShifts,DailySchedule& sched,size_t rows)
+// Helper function to make sure that the worker that will work that day will not overdue the amount of maximum shifts they can do in the span of days.
+bool isValid (const size_t maxShifts, size_t worker,vector<size_t>& workedShifts)
 {
   if(workedShifts[worker] > maxShifts){
     return false;
--- wordle.cpp
+++ wordle.cpp
@@ -15,8 +15,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void potentialWordsHelper (size_t size, std::string& current, const std::set<std::string>& dict, const std::string& floating,std::string& now, std::set<std::string>& options,const std::string& in);
-bool isValid(std::string potentialWord, const std::string& floating,const std::string& in);
+void potentialWordsHelper (size_t position, std::string& current, const std::set<std::string>& dict, const std::string& floating, const std::string& constantFloating, std::set<std::string>& options);
+bool isValid(std::string potentialWord, const std::string& floating);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -25,89 +25,77 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-    std::set<std::string> possibleWords;
     std::string current = in;
-    std::string now = floating;
-    potentialWordsHelper(0, current, dict, floating, now, possibleWords, in);
+    std::set<std::string> possibleWords;
+    const std::string constantFloating = floating;
+    potentialWordsHelper(0, current, dict, floating, constantFloating, possibleWords);
     return possibleWords;
     
 }
 
 // Define any helper functions here
 
-void potentialWordsHelper(size_t position, std::string& current, const std::set<std::string>& dict,const std::string& floating,  std::string& now, std::set<std::string>& options, const std::string& in) 
+/* Recursion function that will go through every index of the word that is covered. Through each iteration a character 
+will be placed in the string until it removes all dashes. Once it removes all dashes it will check if it is a valid word
+and contains all the characters from the floating string.
+*/
+void potentialWordsHelper(size_t position, std::string& current, const std::set<std::string>& dict,const std::string& floating, const std::string& constantFloating, std::set<std::string>& options) 
 {
     if(current.size() == 0){
       return;
     }
     
+    // Check if it is valid word that will go to set options once all dashes are covered.
     if((current.size()) == position)
     {
-      if(isValid(current, now, in)){
+      // First check if it contains all characters from floating string before checking it is a word to be efficient.
+      if(isValid(current, constantFloating)){
+        //Once it is valid, then we can search within the dictionary to see if it is a real word from the list.
         if(dict.find(current) != dict.end()){
+          // After meeting all conditions then we could add the word to the set of final words that could be the answer.
           options.insert(current);
         } 
       }
       return;
     }
 
+    // This will check and go through loops if the current position is a dash thus a character is needed to be placed.
     if(current[position] == '-'){
-       /* for(size_t i = 0; i < floating.size(); ++i){
-          current[position] = floating[i];
-          std::string now = floating;
-          now.erase(i,1);
-          potentialWordsHelper(position + 1, current, dict, now, options);
-        }
+      /* If there are more postions to be covered than floating then it must mean that we must use other characters that 
+        are not the floating ones.
+      */
         if(floating.size() < (current.size() - position)){
           for(char place = 'a'; place <= 'z'; ++place){
-            if(floating.find(place) == std::string::npos) {
+            // skip the characters in the floating string because we will check them later on.
+            if(floating.find(place) == -1) {
               current[position] = place;
-              potentialWordsHelper(position + 1, current, dict, floating, options);
-            } 
-          }
-        }
-        current[position] = '-';*/
-
-        for(size_t i = 0; i < floating.size(); ++i){
-          current[position] = floating[i];
-          std::string on = floating;
-          on.erase(i,1);
-          potentialWordsHelper(position + 1, current, dict, on, now, options, in);
-        }
-        if(floating.size() < (current.size() - position)){
-          for(char place = 'a'; place <= 'z'; ++place){
-            if(floating.find(place) == std::string::npos) {
-              current[position] = place;
-              potentialWordsHelper(position + 1, current, dict, floating,now, options, in); 
+              potentialWordsHelper(position + 1, current, dict, floating, constantFloating, options); 
             }
           }
         }
-        current[position] = '-';
-
-        /*for(char place = 'a'; place <= 'z'; place++){
-           size_t location = now.find(place);
-           if(location != std::string::npos){
-              current[position] = now[location];
-               now.erase(location,1);
-               potentialWordsHelper(position + 1, current, dict,floating, now, options);
-           }
-           else{
-            current[position] = place;
-            potentialWordsHelper(position + 1, current, dict, floating,now, options);
-           }
+        // Then we will place all the characters from the floating string into the dashes so that the string contains all of them.
+        for(size_t i = 0; i < floating.size(); ++i){
+          current[position] = floating[i];
+          std::string temp = floating;
+          // Will remove the character from floating so that we do not repeat it.
+          temp.erase(i,1);
+          potentialWordsHelper(position + 1, current, dict, temp, constantFloating, options);
         }
-        current[position] = '-';*/
+        current[position] = '-';
     }
+    // This is the correct character in place that was given from in string, so we can just iterate to next position.
     else{
-      potentialWordsHelper(position + 1, current, dict, floating,now, options, in);
+      potentialWordsHelper(position + 1, current, dict, floating, constantFloating, options);
     }
 
 }
-bool isValid(std::string potentialWord, const std::string& floating, const std::string& in) 
+
+// This function will make sure that the potential word contains all the characters from floating.
+bool isValid(std::string potentialWord, const std::string& floating) 
 {
   for(size_t i = 0; i < floating.size(); ++i){
     size_t location = potentialWord.find(floating[i]);
-    if(location == std::string::npos){
+    if(location == -1){
       return false;
     }
     potentialWord[location] = '-';
